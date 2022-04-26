3876aa12fee1c877e25cb27931e85de1683e5b5a
Elemi <2323317290@qq.com>
2022-04-26 08:28:02 +0800

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -31,7 +31,7 @@ class HomePageTest(TestCase):
     #     self.assertTrue(html.strip().endswith('</html>'))
     #
     #     self.assertTemplateUsed(response, 'wrong.html')
-    
+
     def test_uses_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
