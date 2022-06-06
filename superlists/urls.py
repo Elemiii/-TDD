fd65aa7532cea9aa0af2d61a5f7c436aafaa4379
Elemi <2323317290@qq.com>
2022-06-06 11:10:39 +0800

    Use Bootstrap to improve layout

--- superlists/urls.py
+++ superlists/urls.py
@@ -23,9 +23,12 @@ Including another URLconf
 from django.conf.urls import url,include
 from lists import views as list_views
 from lists import urls as list_urls
+# from lists import views
+# from lists import urls
 
 urlpatterns=[
     url(r'^$', list_views.home_page, name='home'),
+    # url(r'^$', views.home_page, name='home'),
     # url(r'^lists/new$',views.new_list,name='new_list'),
     # url(r'^lists/(\d+)/$',views.view_list,name='view_list'),
     # url(r'^lists/(\d+)/add_item$',views.add_item,name='add_item'),
